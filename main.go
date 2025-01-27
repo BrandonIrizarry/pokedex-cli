3291e471cce045d9f0880c85796882f5712ddf13
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-01-26 20:58:47 -0500

    Fully implement 'help' and 'exit' commands
    Refactoring the command-registry definition was necessary here, since
    commandHelp, while referenced inside the registry, itself needs the
    registry to be first defined.

--- main.go
+++ main.go
@@ -21,19 +21,38 @@ type cliCommand struct {
 	callback    func() error
 }
 
+var commandRegistry = make(map[string]cliCommand)
+
+// For now, this is just a dummy command.
 func commandExit() error {
 	return nil
 }
 
-var commandRegistry = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
+func commandHelp() error {
+	fmt.Printf("Usage:\n\n")
+	for commandName, clicmd := range commandRegistry {
+		fmt.Printf("%s: %s\n", commandName, clicmd.description)
+	}
+
+	return nil
 }
 
 func main() {
+	// Define the registry here, in main.
+	commandRegistry["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+
+	commandRegistry["help"] = cliCommand{
+		name:        "help",
+		description: "Display names of commands with their descriptions",
+		callback:    commandHelp,
+	}
+
+	fmt.Println("Welcome to the Pokedex!")
+
 	scanner := bufio.NewScanner(os.Stdin)
 	prompt := "Pokedex > "
 
@@ -44,7 +63,23 @@ func main() {
 
 		what := commandRegistry[command]
 
+		// Check for a missing hashmap value. Unfortunately, we can't
+		// compare 'what' directly with 'cliCommand{}', since
+		// 'cliCommand' includes a func field which breaks
+		// comparability.
 		if what.name == "" {
+			break
+		}
+
+		if err := what.callback(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error in command '%s': %v\n", what.name, err)
+		}
+
+		// In case 'commandExit' should contain extra logic in the
+		// future, we check for the presence of the exit command
+		// _after_ running the current command's callback.
+		if what.name == "exit" {
+			fmt.Println("Closing the Pokedex... Goodbye!")
 			break
 		}
 	}
